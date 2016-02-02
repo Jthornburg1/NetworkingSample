23ea5d028805bddc64c9ca54bfef12e14b6f0b66
Jon Thornburg <jon_thornburg@clickherelabs.com>
2016-02-02 16:37:13 -0600

    first commit

---
+++ NetworkingSample/Wrapper.swift
@@ -0,0 +1,15 @@
+//
+//  Wrapper.swift
+//  NetworkingSample
+//
+//  Created by Jon Thornburg on 2/2/16.
+//  Copyright © 2016 Jon Thornburg. All rights reserved.
+//
+
+import UIKit
+
+
+
+class Wrapper: NSObject {
+
+}
